9594b20708011a55576c0a965207cd7e0538c4a0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-03-19 14:26:04 +0100

    Created mergeSort function that only get to divide twice without doing nothing

--- index.js
+++ index.js
@@ -30,3 +30,43 @@ console.log(array);
 */
 
 // PROJECT MERGE SORT
+
+function mergeSort(array) {
+	console.log(array);
+	let media = array.length / 2;
+	if (media >= 2) {
+		let left = [];
+		let right = [];
+		for (let i = 0; i < media; i++) {
+			left.push(array[i]);
+		}
+		for (let i = media; i < array.length; i++) {
+			right.push(array[i]);
+		}
+		console.log(left, right);
+
+		let media2 = media / 2;
+
+		if (media2 >= 2) {
+			let leftLeftPart = media2; // Left
+			let leftRightPart = media2 * 2; // Left
+			let leftLeftPartArray = [];
+			let leftRightPartArray = [];
+			for (let i = 0; i < leftLeftPart; i++) {
+				leftLeftPartArray.push(array[i]);
+			}
+
+			for (let i = leftLeftPart; i < leftRightPart; i++) {
+				leftRightPartArray.push(array[i]);
+			}
+			console.log(leftLeftPartArray, leftRightPartArray);
+
+			if (leftLeftPart >= 2 && leftRightPart >= 2) {
+				return mergeSort(leftLeftPartArray), mergeSort(leftRightPart);
+			}
+		}
+	}
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+//console.log(mergeSort([105, 79, 100, 110]));
